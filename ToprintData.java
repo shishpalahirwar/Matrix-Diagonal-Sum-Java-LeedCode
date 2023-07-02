68895ee2973730741fc0f845b3cf401b39442f73
shishpalahirwar <118547621+shishpalahirwar@users.noreply.github.com>
2023-07-02 23:39:23 +0530

    Add files via upload

---
+++ ToprintData.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        int number = 10;
+        double pi = 3.14;
+        String message = "Hello, World!";
+        
+        // Print the values of the variables
+        System.out.println("Number: " + number);
+        System.out.println("Pi: " + pi);
+        System.out.println("Message: " + message);
+    }
+}
